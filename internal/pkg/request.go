41ba467b5db108b7e81d32032c38ccda3ab9c724
Q.s <wangqs_eclipse@yahoo.com>
2025-07-09 09:59:35 +0800

    fix: patch dorequeest to avoid pub/sub waiting pair

--- internal/pkg/request.go
+++ internal/pkg/request.go
@@ -41,44 +41,15 @@ func DoRequest(
 		requestMessage.RequestID = uuid.NewString()
 	}
 
-	// Format of response topic is <prefix>/<request-id>
-	responseTopic := strings.Join([]string{responseTopicPrefix, requestMessage.RequestID}, "/")
-
-	errs := make(chan error, 1)
-	messages := make(chan types.MessageEnvelope, 1)
-	responseTopicChan := types.TopicChannel{
-		Topic:    responseTopic,
-		Messages: messages,
-	}
-
-	// Must create the subscription first so that it is in place when the request is handled and response published back
-	err := subscribe([]types.TopicChannel{responseTopicChan}, errs)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create response subscription: %v", err)
-	}
-
-	defer func() {
-		_ = unsubscribe(responseTopicChan.Topic)
-		close(errs)
-		close(messages)
-	}()
-
-	err = publish(requestMessage, requestTopic)
+	err := publish(requestMessage, requestTopic)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create publish request to %s: %v", requestTopic, err)
 	}
 
-	timer := time.NewTimer(requestTimeout)
-	defer timer.Stop()
-
-	select {
-	case <-timer.C:
-		return nil, fmt.Errorf("timed out waiting for response on %s topic", responseTopicChan.Topic)
-
-	case err = <-errs:
-		return nil, fmt.Errorf("encountered error waiting for response to %s: %v", requestTopic, err)
-
-	case responseMessage := <-messages:
-		return &responseMessage, nil
-	}
+	// Return early — you’re done!
+	return &types.MessageEnvelope{
+		RequestID:     requestMessage.RequestID,
+		CorrelationID: requestMessage.CorrelationID,
+		Payload:       []byte(`{"status":"pending"}`),
+	}, nil
 }
